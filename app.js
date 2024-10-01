b44042c192352ff6fc430a0def3af8e2f67b6d4c
davibcavalcante <davib506@gmail.com>
2024-10-01 20:25:24 -0300

    Ajustando configuração de porta

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const port = require('./config.json').port
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,6 +13,6 @@ const routes = require('./src/routes/api')
 
 app.use(('/', routes));
 
-app.listen(5000, () => {
-    console.log('Servidor escutanto em http://localhost:5000')
+app.listen(port, () => {
+    console.log(`Servidor escutanto em: ${port}`)
 })
